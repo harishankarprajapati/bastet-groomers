844131d52a3507bf64fafd1f890d76ea7e29e95b
harishankarprajapati <pharishankar1453@gmail.com>
2023-02-03 17:15:15 +0530

    fdgfg

--- js/js.js
+++ js/js.js
@@ -1,16 +1,6 @@
-$(function () {
-    setNavigation();
-  });
-  
-  function setNavigation() {
-    var path = window.location.pathname;
-    path = path.replace(/\//g, "");
-    path = decodeURIComponent(path);
-    
-    $("#menu-main-menu a").each(function () {
-        var href = $(this).attr('href');
-        if (path.substring(0, href.length) === href) {
-            $(this).closest('li').addClass('active');
-        }
-    });
-  }
+$(document).ready(function(){
+  $('ul li a').click(function(){
+    $('li a').removeClass("active");
+    $(this).addClass("active");
+});
+});
